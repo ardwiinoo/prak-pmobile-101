bdb7b47219fb2fa74f4045a9f6cb12dd245d5b60
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-09-29 13:57:55 +0700

    init

---
+++ app/src/main/java/com/ardwiinoo/prak_pmobile_101/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ardwiinoo.prak_pmobile_101
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
