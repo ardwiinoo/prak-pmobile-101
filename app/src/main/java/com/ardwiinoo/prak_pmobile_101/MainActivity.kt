97661dba80e56d23ce7b292d775cce5597cea86b
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2023-10-17 18:20:24 +0700

    disable viewBinding for reason

--- app/src/main/java/com/ardwiinoo/prak_pmobile_101/MainActivity.kt
+++ app/src/main/java/com/ardwiinoo/prak_pmobile_101/MainActivity.kt
@@ -2,10 +2,46 @@ package com.ardwiinoo.prak_pmobile_101
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+//import com.ardwiinoo.prak_pmobile_101.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+//    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.sample_quiz)
+
+        Log.d(TAG, "onCreate")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.d(TAG, "onDestroy")
+    }
+
+    companion object {
+        val TAG: String = MainActivity::class.java.simpleName
     }
 }
